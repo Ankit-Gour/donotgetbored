54b22c8ad82e2d2af0c8cf40c336147900f53c14
Ankit-Gour <127835812+Ankit-Gour@users.noreply.github.com>
2023-06-26 17:16:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+act();
+var prev_type="";
+function act() {
+  document.getElementById("act").innerHTML = ``;
+  fetch("https://www.boredapi.com/api/activity")
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+        let activity =   data.activity.charAt(0).toUpperCase() + data.activity.slice(1);
+        let type = data.type.charAt(0).toUpperCase() + data.type.slice(1);
+        
+        document.getElementById("act").innerHTML = `${activity}`;
+        document.getElementById("type").innerHTML = `${type}`;
+
+        
+      
+    });
+}
+
+setInterval(() => {
+    act()
+}, 3000);
+
+
+
+
+setTimeout(() => {
+    gif.src="happy.gif"
+}, 2000);
+
+setTimeout(() => {
+    gif.src="baby.jpg"
+    gif.style=`width:200px`
+}, 4000);
+
+setTimeout(() => {
+    gif.src="bored.gif"
+    gif.style=`width:50px margin:10px auto;`
+}, 6000);
+setInterval(() => {
+    
+    setTimeout(() => {
+        gif.src="happy.gif"
+    }, 2000);
+    
+    setTimeout(() => {
+        gif.src="baby.jpg"
+        gif.style=`width:200px`
+    }, 4000);
+    
+    setTimeout(() => {
+        gif.src="bored.gif"
+        gif.style=`width:50px`
+    }, 6000);
+    
+
+},7000);
